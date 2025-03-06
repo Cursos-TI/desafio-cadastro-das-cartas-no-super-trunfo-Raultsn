8e3c3aaaa68d5e2130fec64239d188da09e9ef2f
Raultsn <rauldantas561@gmail.com>
2025-03-06 17:45:06 +0000

    DESAFIO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
-
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
+    char estado = 'A'; // Caracter único aspas simples 
+    char codigo[] = "E01"; // Mais de um caracter aspas duplas 
+    char Nome[] = "Rio de Janeiro"; // Mais de um caracter aspas duplas 
+    int populacao = 2500000; 
+    float area = 2500.000; 
+    float PIB = 50000000; 
+    int NPT = 15; // NPT = Número de Pontos Turísticos
+
+    // Impressão das variáveis
+    printf("Estado é: %c\n", estado);
+    printf("Código é: %s\n", codigo);
+    printf("Nome é: %s\n", Nome); 
+    printf("População é: %d\n", populacao);
+    printf("Área é: %.2f\n", area); // %.2f para melhor vizualização
+    printf("PIB é: %.1f\n", PIB); 
+    printf("NPT é: %d\n", NPT); // NPT = Número de Pontos Turísticos
+   
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    char estado2 = 'B'; 
+    char codigo2[] = "E02";
+    char Nome2[] = "São Vicente "; 
+    int populacao2 = 100000;
+    float area2 = 3500.000; 
+    float PIB2 = 30000000; 
+    int NPT2 = 20; 
+  
+
+    printf("Estado é: %c\n", estado2);
+    printf("Código é: %s\n", codigo2);
+    printf("Nome é: %s\n", Nome2); 
+    printf("População é: %d\n", populacao2);
+    printf("Área é: %.2f\n", area2); // %.2f para melhor vizualização 
+    printf("PIB é: %.1f\n", PIB2); 
+    printf("NPT é: %d\n", NPT2); // NPT = Número de Pontos Turísticos
+
+
 
     return 0;
 }
